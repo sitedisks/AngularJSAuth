10ef98567bf168086ee82ae4c98e430e49e55137
Peter <peter@sitedisks.com.au>
2015-03-29 10:17:55 +1100

    angular app config, run and Factory (connect to Auth API)

--- AngularJSAuth.Web/app/app.js
+++ AngularJSAuth.Web/app/app.js
@@ -7,6 +7,34 @@
         'ngAnimate'
     ]);
 
-app.controller('testCtrl', function ($scope) {
-    $scope.message = 'Love you';
+//app.controller('testCtrl', function ($scope) {
+//    $scope.message = 'Love you';
+//});
+
+app.config(function ($routeProvider) {
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "/app/views/home.html"
+    });
+
+    $routeProvider.when("/login", {
+        controller: "loginController",
+        templateUrl: "/app/views/login.html"
+    });
+
+    $routeProvider.when("/signup", {
+        controller: "signupController",
+        templateUrl: "/app/views/signup.html"
+    });
+
+    $routeProvider.when("/orders", {
+        controller: "ordersController",
+        templateUrl: "/app/views/orders.html"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/home" });
 });
+
+app.run(['authService', function (authService) {
+    //authService.fillAuthData();
+}]);
