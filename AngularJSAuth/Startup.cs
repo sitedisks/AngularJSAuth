056f5cf03f29ab42671c39f74030c139226368a0
Peter <peter@sitedisks.com.au>
2015-03-28 17:51:52 +1100

    OAuth config

--- AngularJSAuth/Startup.cs
+++ AngularJSAuth/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Owin;
+﻿using AngularJSAuth.Providers;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
 using System;
 using System.Collections.Generic;
@@ -11,11 +13,29 @@ namespace AngularJSAuth.API
 {
     public class Startup
     {
+
         public void Configuration(IAppBuilder app) {
 
+            ConfigureOAuth(app);
             HttpConfiguration config = new HttpConfiguration();
             WebApiConfig.Register(config);
             app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
         }
     }
 }
