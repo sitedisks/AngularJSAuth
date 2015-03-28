227176bfc1704ef50a87a038433b8e37c0da0917
Peter <peter@sitedisks.com.au>
2015-03-28 22:19:48 +1100

    angular XHR loading bar and Angular animate package

--- AngularJSAuth.Web/app/app.js
+++ AngularJSAuth.Web/app/app.js
@@ -1,4 +1,11 @@
-﻿var app = angular.module('siteApp', ['ui.bootstrap', 'ngRoute', 'LocalStorageModule']);
+﻿var app = angular.module('siteApp',
+    [
+        'ui.bootstrap',
+        'ngRoute',
+        'LocalStorageModule',
+        'angular-loading-bar',
+        'ngAnimate'
+    ]);
 
 app.controller('testCtrl', function ($scope) {
     $scope.message = 'Love you';
