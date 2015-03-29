536ca63dde2a685433062b687d787214c0f90ea8
Peter <peter@sitedisks.com.au>
2015-03-29 22:15:30 +1100

    order controller (test)

---
+++ AngularJSAuth.Web/app/controllers/ordersCtrl.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+
+    app.controller('ordersCtrl', OrdersCtrl);
+
+    OrdersCtrl.$inject = ['ordersSvc'];
+
+    function OrdersCtrl(ordersSvc) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.orders = [];
+
+        ordersSvc.getOrders().then(function (results) {
+
+            vm.orders = results.data;
+
+        }, function (error) {
+            //alert(error.data.message);
+        });
+    }
+})();
