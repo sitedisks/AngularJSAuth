9a1226f1a7dd179883cb850a6f86e919f0f3e9c9
Peter <peter@sitedisks.com.au>
2015-04-06 11:42:14 +1000

    adjust the login and menu side

---
+++ AngularJSAuth.Web/app/controllers/statusCtrl.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    app.controller('statusCtrl', StatusCtrl);
+
+    StatusCtrl.$inject = ['$location', 'authService'];
+
+    function StatusCtrl($location, authService) {
+        /* jshint validthis:true */
+        var vm = this;
+
+        vm.logOut = function () {
+            authService.logOut();
+            $location.path('/home');
+        }
+
+        vm.authentication = authService.authentication;
+    }
+})();
